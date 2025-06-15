9dbf896e62b9e071f7ff01d74e2452dcd1f74bb0
Meathill <meathill@gmail.com>
2025-06-15 18:46:23 +0800

    bug: fixed project card

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link' // Import Link
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { ShieldCheck } from 'lucide-react'
 import { ProjectInfo } from '@/types';
 
 type ProjectCardProps = {
@@ -9,13 +8,16 @@ type ProjectCardProps = {
 }
 
 export function ProjectCard({ project }: ProjectCardProps) {
+  const progress = Math.round(project.amount / project.goal_amount * 1000) / 10;
+  const daysLeft = Math.ceil((new Date(project.end_at).getTime() - Date.now()) / 1000 / 60 / 60 / 24);
+
   return (
     <Link
       href={`/project/${project.project_slug}`}
       className="block hover:shadow-lg transition-shadow duration-800 rounded-lg h-full drop-shadow-custom1"
     >
       <Card className="group overflow-hidden flex flex-col h-full border-transparent hover:border-action-blue/50 transition-colors">
-        <div className="relative w-full aspect-[16/9] h-[405px] group-hover:h-[60%] transition-height duration-800">
+        <div className="relative w-full aspect-video group-hover:aspect-[5/2] transition-all duration-800">
           <Image
             src={project.banner || '/images/placeholder.svg'}
             alt={project.project_name}
@@ -29,19 +31,27 @@ export function ProjectCard({ project }: ProjectCardProps) {
           <p className="text-xs text-black/70 text-neutral-darkgray mt-1 line-clamp-1 group-hover:line-clamp-2">{project.description}</p>
         </CardHeader>
         <CardContent className="flex-grow">
-          <div className="flex items-center text-xs">
-            <ShieldCheck className="h-4 w-4 mr-1.5 text-intuipay-blue" />
-            <span>{project.university_name}</span>
+          <div className="flex items-center text-xs gap-2">
+            <div className="p-1 rounded-full border">
+              <Image
+                className="block size-6"
+                src={project.org_logo}
+                alt={project.org_name}
+                width={24}
+                height={24}
+              />
+            </div>
+            <span>{project.org_name}</span>
           </div>
         </CardContent>
         <CardFooter className="mx-5 pt-4 border-t border-neutral-mediumgray/30 whitespace-nowrap overflow-hidden">
           <div className="flex justify-between items-center">
             <span className="text-xs text-neutral-darkgray">Total Raised</span>
             <span className="text-base font-bold text-neutral-text text-ellipsis">
-              $ {project.goal_amount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
+              $ {(project.amount / 100).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}
             </span>
           </div>
-          <p className="text-sm text-black mt-4 hidden opacity-0 group-hover:block group-hover:opacity-60 transition-opacity duration-800">xxx days left • xx% founded</p>
+          <p className="text-sm text-black mt-4 hidden opacity-0 group-hover:block group-hover:opacity-60 transition-opacity duration-800">{daysLeft} days left • {progress}% founded</p>
         </CardFooter>
       </Card>
     </Link>
--- types/index.ts
+++ types/index.ts
@@ -65,7 +65,12 @@ export type ProjectInfo = {
   project_slug: string;
   description: string;
   banner: string;
+  amount: number;
   goal_amount: number;
+  end_at: string;
+  org_name: string;
+  org_slug: string;
+  org_logo: string;
 }
 
 export type ProjectFilter = {
