303babacfd1b35878b49ba25f358b8f35dfa289e
Joey <joey.xf@gmail.com>
2025-06-25 11:05:54 +0800

    parse tidb string to json

--- lib/data.ts
+++ lib/data.ts
@@ -7,17 +7,10 @@ export const getDonationProjectBySlug = cache(async function getDonationProjectB
   const data = await fetchTidb<DonationProject>(`/donation_project?slug=${slug}`);
   const project = data[ 0 ];
 
-  // 在tidb准备好之前，本地写死一些数据测试，数据库好了再删掉
-  // project.networks = ['ethereum-sepolia', 'solana-devnet'];
-  // project.tokens = {
-  //   'ethereum-sepolia': ['eth', 'usdc'],
-  //   'solana-devnet': ['sol'],
-  // }
-  // project.wallets = {
-  //   'ethereum-sepolia': '0x1234567890abcdef1234567890abcdef12345678',
-  //   'solana-devnet': 'Ft7m7qrY3spLNKo6aMAHMArAT3oLSSy4DnJ3y3SF1DP1',
-  // }
+  // tidb endpoint返回的是string，所以需要手动转为json
+  project.networks = JSON.parse((project.networks as unknown) as string);
+  project.tokens = JSON.parse((project.tokens as unknown) as string);
+  project.wallets = JSON.parse((project.wallets as unknown) as string);
 
-  // console.log('getDonationProjectBySlug', slug, project);
   return project;
 });
