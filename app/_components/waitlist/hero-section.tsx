8fca06103f990aa5fc9e8caa03a1f38cbcf3f53d
Joey <joey.xf@gmail.com>
2025-07-27 22:59:04 +0800

    add more padding to hero section

--- app/_components/waitlist/hero-section.tsx
+++ app/_components/waitlist/hero-section.tsx
@@ -3,9 +3,9 @@ import PaymentDemo from '@/components/payment-demo'
 export default function HeroSection() {
   return (
     <section className="xl:py-16 xl:px-16 2xl:px-20">
-      <div className="bg-brand-blue xl:rounded-3xl px-8 py-12 md:p-12 lg:px-16 lg:py-8 xl:px-28 xl:py-8">
+      <div className="bg-brand-blue xl:rounded-3xl px-8 py-12 md:p-12 lg:px-16 lg:py-24 xl:px-28">
         <div className="xl:max-w-6xl 2xl:max-w-8xl mx-auto flex flex-col lg:flex-row lg:items-center gap-8 md:gap-12 lg:gap-16">
-          <div className="lg:w-1/2 flex-none">
+          <div className="flex-1">
             <h1 className="text-3xl md:text-6xl font-medium font-neue-montreal mb-6 leading-tight">
               Where{' '}
               <span className="inline-flex items-center align-top w-20 relative me-2" aria-label="Flags">
@@ -40,7 +40,7 @@ export default function HeroSection() {
             </div>
           </div>
 
-          <div className="lg:w-1/2 flex-none">
+          <div className="flex-none">
             <div className="w-full max-w-md mx-auto p-6 md:p-8 bg-white rounded-2xl shadow-sm border border-gray-100">
               <PaymentDemo />
             </div>
