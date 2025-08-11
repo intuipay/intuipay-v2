74f451a85e51e82b06047d18fe7bc66c2d6b5064
Joey <joey.xf@gmail.com>
2025-08-11 21:29:30 +0800

    should also display current user in /projects page site-header

--- app/(projects)/layout.tsx
+++ app/(projects)/layout.tsx
@@ -1,16 +1,21 @@
 import { SiteHeader } from '@/components/site-header'
 import { SiteFooter } from '@/components/site-footer'
+import { auth } from "@/lib/auth";
+import { headers } from 'next/headers';
 
+export const runtime = 'edge';
 
 export default async function ProjectsLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   return (
     <div className="flex flex-col min-h-screen bg-neutral-white text-neutral-text">
-      <SiteHeader />
+      <SiteHeader user={session?.user} />
       <main className="w-full max-w-7xl mx-auto flex-grow px-12 md:px-10 py-20 sm:py-20">
         {children}
       </main>
