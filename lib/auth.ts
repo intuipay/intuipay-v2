9c8f3ca5e61d5ed603952e3f369c7cddb5676658
Joey <joey.xf@gmail.com>
2025-08-13 16:29:33 +0800

    feature: locally auth api

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,14 +2,22 @@ import { betterAuth } from 'better-auth';
 import { db } from './db';
 
 export const auth = betterAuth({
-  baseURL: process.env.NEXT_PUBLIC_DASHBOARD_URL,
+  baseURL: process.env.NEXT_PUBLIC_DASHBOARD_URL || 'http://localhost:3000',
   advanced: {
     crossSubDomainCookies: {
-      enabled: true
+      enabled: true,
     },
   },
   database: {
     db,
     type: 'sqlite',
   },
+  trustedOrigins: [
+    "https://intuipay.xyz",
+    "https://dash.intuipay.xyz",
+    "https://dev.intuipay.xyz",
+    "https://dash.dev.intuipay.xyz",
+    "https://crowdfunding.dev.intuipay.xyz",
+    "http://localhost:3000",
+  ],
 })
