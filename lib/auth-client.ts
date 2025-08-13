5be92c1ea45ca5c690cf3022a00d261750691dae
Joey <joey.xf@gmail.com>
2025-08-13 16:29:33 +0800

    feature: locally auth api

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,4 @@
 import { createAuthClient } from 'better-auth/react'
 
-// 根据环境变量决定使用模拟还是真实的 authClient
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_DASHBOARD_URL || 'http://localhost:3000'
 })
