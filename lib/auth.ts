cf34fb076ccaf9ee3ce01bb68d12eea145745efd
Joey <joey.xf@gmail.com>
2025-08-13 11:38:25 +0800

    use baseURL to dashboard url

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from 'better-auth';
 import { db } from './db';
 
 export const auth = betterAuth({
-  baseURL: process.env.NEXT_PUBLIC_WEBSITE_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_DASHBOARD_URL || 'http://localhost:3000',
   advanced: {
     crossSubDomainCookies: {
       enabled: true,
