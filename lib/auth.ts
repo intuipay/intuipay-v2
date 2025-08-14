b78ec2466cef7dea4ad6de95ac3de4fdd0ca123a
Meathill <meathill@gmail.com>
2025-08-15 00:50:25 +0800

    change: updated worker name

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,19 @@
 import { betterAuth } from 'better-auth';
-import { db } from './db';
+import { getCloudflareContext } from '@opennextjs/cloudflare';
+import { Kysely } from 'kysely';
+import { D1Dialect } from 'kysely-d1';
+
+async function initDb() {
+  const context = await getCloudflareContext({ async: true });
+
+  return new Kysely({
+    dialect: new D1Dialect({
+      database: context.env.DB,
+    }),
+  });
+}
+
+const db = await initDb();
 
 export const auth = betterAuth({
   baseURL: process.env.NEXT_PUBLIC_DASHBOARD_URL || 'http://localhost:3000',
@@ -13,11 +27,11 @@ export const auth = betterAuth({
     type: 'sqlite',
   },
   trustedOrigins: [
-    "https://intuipay.xyz",
-    "https://dash.intuipay.xyz",
-    "https://dev.intuipay.xyz",
-    "https://dash.dev.intuipay.xyz",
-    "https://crowdfunding.dev.intuipay.xyz",
-    "http://localhost:3000",
+    'https://intuipay.xyz',
+    'https://dash.intuipay.xyz',
+    'https://dev.intuipay.xyz',
+    'https://dash.dev.intuipay.xyz',
+    'https://crowdfunding.dev.intuipay.xyz',
+    'http://localhost:3000',
   ],
 })
