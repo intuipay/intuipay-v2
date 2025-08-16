ad2773d8c803e74910429778ed16f56549f715f6
Joey <joey.xf@gmail.com>
2025-08-16 14:34:03 +0800

    send email after donation and refund (#52)
    * send donate email
    
    * send refund email
    
    * fix data in donation and refund email
    
    1. call api to get current user's refund amount
    2. pass donation index to domation email
    
    * not running refund test on default
    
    * accept copilot fixes
    
    * optimize code style
    
    * change tabsize

---
+++ __tests__/refund.test.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest";
+import { getUserRefund } from "@/lib/data";
+
+describe('Test Refund', () => {
+  describe('getUserRefund', () => {
+    // process.loadEnvFile('./.env'); // 本地测试时开启
+
+    it.skipIf(!process.env.TIDB_CLOUD_API_KEY)('should return user refund information', async () => {
+      const userId = 'jXqDtVMvNv1vf81izMoLabAkoOlQX5P1';
+      const projectId = 120002;
+      const refund = await getUserRefund(userId, projectId);
+      console.log('refund info', refund);
+
+      // { amount: 0, count: 1, dollar: 100 }
+      expect(refund).toHaveProperty('amount');
+      expect(refund).toHaveProperty('count');
+      expect(refund).toHaveProperty('dollar');
+    });
+  });
+});
