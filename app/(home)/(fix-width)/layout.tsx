8395ad5cbf47360e6777b3bc6964d6a060e0a94d
Meathill <meathill@gmail.com>
2025-08-22 10:02:49 +0800

    bug: removed wrong px

--- app/(home)/(fix-width)/layout.tsx
+++ app/(home)/(fix-width)/layout.tsx
@@ -5,7 +5,7 @@ export default function FixWidthLayout({
   children
 }: PropsWithChildren) {
   return <>
-    <main className="w-full container mx-auto flex-grow px-12 md:px-10 pt-20">
+    <main className="w-full container mx-auto flex-grow md:pt-20">
       {children}
     </main>
     <SiteFooter />
