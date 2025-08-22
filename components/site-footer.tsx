eae077b6df25a9c5caa5ffa8ee7b320e02dd82d2
Joey <joey.xf@gmail.com>
2025-08-22 12:59:42 +0800

    keep align with footer-links

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -9,7 +9,7 @@ export function SiteFooter({
 }: Props) {
   return (
     <footer className={clsx('py-4', hasDivider ? 'border-t' : 'bg-brand-blue-100 md:py-16')}>
-      <div className="w-full max-w-7xl px-12 md:px-10 mx-auto">
+      <div className="px-8 xl:max-w-6xl 2xl:max-w-8xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center md:items-end text-center md:text-left">
           <p className="text-xs text-neutral-darkgray text-[#737373]">
             &copy; {new Date().getFullYear()} Intuipay Holding PTE. LTD. All rights reserved.
