50a8f357aeab408d1622ca3713f73b46037bd467
Joey <joey.xf@gmail.com>
2025-08-22 17:25:47 +0800

    footer copyright text should align left

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -10,7 +10,7 @@ export function SiteFooter({
   return (
     <footer className={clsx('py-4', hasDivider ? 'border-t' : 'bg-brand-blue-100 md:py-16')}>
       <div className="px-8 xl:max-w-6xl 2xl:max-w-8xl mx-auto">
-        <div className="flex flex-col md:flex-row justify-between items-center md:items-end text-center md:text-left">
+        <div className="flex flex-col md:flex-row justify-between items-center md:items-end text-left">
           <p className="text-xs text-neutral-darkgray text-[#737373]">
             &copy; {new Date().getFullYear()} Intuipay Holding PTE. LTD. All rights reserved.
             <span className="text-neutral-darkgray ms-2">v{process.env.NEXT_PUBLIC_APP_VERSION}</span>
